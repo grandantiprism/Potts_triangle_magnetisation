7503391dc5b8bbb5d389f607cb018420dace8ae9
ADE <ackielilac.g1@gmail.com>
2026-01-11 23:08:00 +0900

    Initial Commit

---
+++ Potts_triangle_magnetisation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Potts_triangle_magnetisation
+//
+//  Created by Akihisa Tateishi on 2026/01/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
